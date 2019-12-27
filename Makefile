8c7811bfd21dd8002e039844bf151e3db0e23cd6
Irek Szcześniak <irek.szczesniak@gmail.com>
2019-12-27 13:50:31 +0100

    ee

--- Makefile
+++ Makefile
@@ -10,7 +10,11 @@ CXXFLAGS := $(CXXFLAGS) -O2
 # CXXFLAGS := $(CXXFLAGS) -O2 -D NDEBUG
 
 CXXFLAGS := $(CXXFLAGS) -std=c++17
+CXXFLAGS := $(CXXFLAGS) -I .
 CXXFLAGS := $(CXXFLAGS) -I des/include
+CXXFLAGS := $(CXXFLAGS) -I dijkstra
+CXXFLAGS := $(CXXFLAGS) -I generic_dijkstra
+CXXFLAGS := $(CXXFLAGS) -I standard_dijkstra
 
 # Use the C++ linker
 LINK.o = $(LINK.cc)
--- ee.cc
+++ ee.cc
@@ -1,25 +1,66 @@
-#include "adaptive_units.hpp"
 #include "graph.hpp"
-#include "label.hpp"
-#include "shared.hpp"
-#include "units.hpp"
-#include "utils.hpp"
+
+#include "adaptive_units.hpp"
+#include "generic_dijkstra.hpp"
+#include "generic_label_creator.hpp"
+#include "generic_label.hpp"
+#include "generic_permanent.hpp"
+#include "generic_tentative.hpp"
+#include "generic_tracer.hpp"
 
 #include <optional>
-#include <queue>
 #include <utility>
 
 using namespace std;
+
+optional<cupath>
+search(const graph &g, const demand &d, const CU &cu)
+{
+  vertex src = d.first.first;
+  vertex dst = d.first.second;
+  // The number of contiguous units.
+  int ncu = d.second;
+
+  assert (src != dst);
+
+  // The generic permanent solution type.
+  using per_type = generic_permanent<graph, COST, CU>;
+  // The generic tentative solution type.
+  using ten_type = generic_tentative<graph, COST, CU>;
+
+  // The permanent and tentative solutions.
+  per_type P(boost::num_vertices(g));
+  ten_type T(boost::num_vertices(g));
+  // The label we start the search with.
+  generic_label<graph, COST, CU> l(0, CU(cu), edge(), src);
+  // The creator of the labels.
+  generic_label_creator<graph, COST, CU> c(g, ncu);
+  // Run the search.
+  dijkstra(g, P, T, l, c, dst);
+  // The tracer.
+  generic_tracer<graph, cupath, per_type, CU> t(g, ncu);
+  // Get the path.
+  auto op = trace(P, dst, l, t);
+
+  return op;
+}
 
 std::optional<std::pair<cupath, cupath> >
 ee(const graph &g, const demand &d, const CU &cu)
 {
   std::optional<std::pair<cupath, cupath> > result;
 
-  const vertex &src = d.first.first;
-  const vertex &dst = d.first.second;
-  // The number of contiguous units.
-  const unsigned &ncu = d.second;
+  // We have to run the algorithm twice: first without filtering, then
+  // with filtering.
+  auto first = search(g, d, cu);
 
-  return result;
+  if (first)
+    {
+      auto second = search(g, d, cu);
+
+      if (second)
+        return make_pair(first.value(), second.value());
+    }
+
+  return {};
 }
