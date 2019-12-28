da7f685f9bbeb27ef8332a9e670252ed28665752
Irek Szcześniak <iszczesniak@peter>
2019-12-28 23:00:03 +0100

    ee

--- ee.cc
+++ ee.cc
@@ -1,3 +1,5 @@
+#include <boost/graph/filtered_graph.hpp>
+
 #include "graph.hpp"
 
 #include "adaptive_units.hpp"
@@ -10,12 +12,14 @@
 #include "utils.hpp"
 
 #include <optional>
+#include <set>
 #include <utility>
 
 using namespace std;
 
+template<typename Graph>
 optional<cupath>
-search(const graph &g, const demand &d, const CU &cu)
+search(const Graph &g, const demand &d, const CU &cu)
 {
   vertex src = d.first.first;
   vertex dst = d.first.second;
@@ -25,21 +29,21 @@ search(const graph &g, const demand &d, const CU &cu)
   assert (src != dst);
 
   // The generic permanent solution type.
-  using per_type = generic_permanent<graph, COST, CU>;
+  using per_type = generic_permanent<Graph, COST, CU>;
   // The generic tentative solution type.
-  using ten_type = generic_tentative<graph, COST, CU>;
+  using ten_type = generic_tentative<Graph, COST, CU>;
 
   // The permanent and tentative solutions.
   per_type P(boost::num_vertices(g));
   ten_type T(boost::num_vertices(g));
   // The label we start the search with.
-  generic_label<graph, COST, CU> l(0, CU(cu), edge(), src);
+  generic_label<Graph, COST, CU> l(0, CU(cu), edge(), src);
   // The creator of the labels.
-  generic_label_creator<graph, COST, CU> c(g, ncu);
+  generic_label_creator<Graph, COST, CU> c(g, ncu);
   // Run the search.
   dijkstra(g, P, T, l, c, dst);
   // The tracer.
-  generic_tracer<graph, cupath, per_type, CU> t(g, ncu);
+  generic_tracer<Graph, cupath, per_type, CU> t(g, ncu);
   // Get the path.
   auto op = trace(P, dst, l, t);
 
@@ -71,7 +75,20 @@ ee(const graph &g, const demand &d, const CU &cu)
 
   if (first)
     {
-      auto second = search(g, d, cu);
+      // Excluded edges.
+      set<edge> ee;
+
+      for(const auto &e: first.value().second)
+        ee.insert(e);
+
+      // The filtered graph type.
+      using fg_type =
+        boost::filtered_graph<graph,
+                              boost::is_not_in_subset<set<edge>>>;
+
+      fg_type fg(g, ee);
+
+      auto second = search(fg, d, cu);
 
       if (second)
         return make_pair(first.value(), second.value());
